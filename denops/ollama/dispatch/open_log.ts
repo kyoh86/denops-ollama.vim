5e0829c7bb1cf660f6de69d280d55b15ea556b4e
kyoh86 <me@kyoh86.dev>
2024-01-27 16:56:57 +0900

    merge args from store

---
+++ denops/ollama/dispatch/open_log.ts
@@ -0,0 +1,20 @@
+import { Denops } from "https://deno.land/x/denops_std@v5.3.0/mod.ts";
+
+import {
+  is,
+  PredicateType,
+} from "https://deno.land/x/unknownutil@v3.14.1/mod.ts";
+import { isOpener } from "../ui/open.ts";
+
+export const isOpenLogArgs = is.ObjectOf({
+  opener: is.OptionalOf(isOpener),
+});
+export type OpenLogArgs = PredicateType<typeof isOpenLogArgs>;
+
+export async function openLog(
+  denops: Denops,
+  cacheFile: string,
+  args: OpenLogArgs,
+) {
+  await denops.cmd(`${args?.opener ?? "tabnew"} ${cacheFile}`);
+}
--- denops/ollama/main.ts
+++ denops/ollama/main.ts
@@ -1,5 +1,8 @@
 import { Denops } from "https://deno.land/x/denops_std@v6.0.1/mod.ts";
-import { ensure, is } from "https://deno.land/x/unknownutil@v3.15.0/mod.ts";
+import {
+  ensure,
+  Predicate,
+} from "https://deno.land/x/unknownutil@v3.15.0/mod.ts";
 
 import { init } from "./dispatch/init.ts";
 import { isStartChatArgs, startChat } from "./dispatch/start_chat.ts";
@@ -11,35 +14,40 @@ import {
   startChatWithContext,
 } from "./dispatch/start_chat_with_context.ts";
 import { complete, isCompleteArgs } from "./dispatch/complete.ts";
-import { isOpener } from "./ui/open.ts";
+import { CustomArgStore, isArgs } from "./custom/arg_store.ts";
+import { isOpenLogArgs, openLog } from "./dispatch/open_log.ts";
 
 export async function main(denops: Denops) {
   const { cacheFile } = await init(denops);
 
+  const argStore = new CustomArgStore();
+
+  function getArgs<T>(func: string, uArgs: unknown, pred: Predicate<T>): T {
+    return ensure(argStore.getMerged(func, ensure(uArgs, isArgs)), pred);
+  }
+
   denops.dispatcher = {
     async openLog(uArgs: unknown) {
-      const args = ensure(
-        uArgs,
-        is.ObjectOf({
-          opener: is.OptionalOf(isOpener),
-        }),
-      );
-      await denops.cmd(`${args?.opener ?? "tabnew"} ${cacheFile}`);
+      const args = getArgs("openLog", uArgs, isOpenLogArgs);
+      await openLog(denops, cacheFile, args);
     },
 
     async startChat(uArgs: unknown) {
-      await startChat(denops, ensure(uArgs, isStartChatArgs));
+      const args = getArgs("startChat", uArgs, isStartChatArgs);
+      await startChat(denops, args);
     },
 
     async startChatWithContext(uArgs: unknown) {
-      await startChatWithContext(
-        denops,
-        ensure(uArgs, isStartChatWithContextArgs),
+      const args = getArgs(
+        "startChatWithContext",
+        uArgs,
+        isStartChatWithContextArgs,
       );
+      await startChatWithContext(denops, args);
     },
 
     async complete(uArgs: unknown) {
-      const args = ensure(uArgs, isCompleteArgs);
+      const args = getArgs("complete", uArgs, isCompleteArgs);
       await complete(
         denops,
         {
@@ -52,15 +60,18 @@ export async function main(denops: Denops) {
     },
 
     async listModels(uArgs: unknown) {
-      await listModels(denops, ensure(uArgs, isListModelsArgs));
+      const args = getArgs("listModels", uArgs, isListModelsArgs);
+      await listModels(denops, args);
     },
 
     async pullModel(uArgs: unknown) {
-      await pullModel(denops, ensure(uArgs, isPullModelArgs));
+      const args = getArgs("pullModel", uArgs, isPullModelArgs);
+      await pullModel(denops, args);
     },
 
     async deleteModel(uArgs: unknown) {
-      await deleteModel(denops, ensure(uArgs, isDeleteModelArgs));
+      const args = getArgs("deleteModel", uArgs, isDeleteModelArgs);
+      await deleteModel(denops, args);
     },
   };
 }
