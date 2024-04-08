251953e45e3048656ccb0b388dbbe6de72c1d82d
GitHub <noreply@github.com>
2024-04-08 04:11:16 +0000

    chore(deps): Update deno dependencies

--- denops/ollama/dispatch/list_models.ts
+++ denops/ollama/dispatch/list_models.ts
@@ -2,7 +2,7 @@ import * as datetime from "https://deno.land/std@0.221.0/datetime/mod.ts";
 import * as bytes from "https://deno.land/std@0.221.0/fmt/bytes.ts";
 import { Denops } from "https://deno.land/x/denops_std@v6.4.0/mod.ts";
 import * as helper from "https://deno.land/x/denops_std@v6.4.0/helper/mod.ts";
-import { Table } from "https://deno.land/x/cliffy@v1.0.0-rc.3/table/mod.ts";
+import { Table } from "https://deno.land/x/cliffy@v1.0.0-rc.4/table/mod.ts";
 
 import { listLocalModels } from "../api.ts";
 import { isReqArgs, ReqArgs } from "./types.ts";
